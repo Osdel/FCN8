45ac3a36bdcb33075620d2946551ec88aee8ee08
Osdel Hernandez <osdel.hhdez@gmail.com>
2020-03-26 15:21:36 -0400

    Added width and height as parameters to mobilenet_unet

--- unet_mobilenet.py
+++ unet_mobilenet.py
@@ -7,8 +7,16 @@ For more information visit: https://www.tensorflow.org/tutorials/images/segmenta
 import tensorflow as tf
 from tensorflow_examples.models.pix2pix import pix2pix
 
-def unet_model(output_channels):
-  base_model = tf.keras.applications.MobileNetV2(input_shape=[512, 512, 3], include_top=False)
+def unet_model(output_channels,h,w):
+  """
+  Parameters:
+    output_channels: Number of classes to predict
+    h: Height of Images
+    w: Width if Images
+  Output:
+    tf.keras Model instance
+  """
+  base_model = tf.keras.applications.MobileNetV2(input_shape=[h, w, 3], include_top=False)
   # Use the activations of these layers
   layer_names = [
   'block_1_expand_relu',   # 64x64
@@ -30,7 +38,7 @@ def unet_model(output_channels):
   pix2pix.upsample(128, 3),  # 16x16 -> 32x32
   pix2pix.upsample(64, 3),   # 32x32 -> 64x64
   ]
-  inputs = tf.keras.layers.Input(shape=[512, 512, 3])
+  inputs = tf.keras.layers.Input(shape=[h, w, 3])
   x = inputs
 
   # Downsampling through the model
